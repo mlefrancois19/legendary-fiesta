e92fb9be80d1e8140c501656aa1600f0fab2a221
Mathieu LeFrançois <mlefrancois@fungo.ca>
2021-10-02 13:26:24 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(title: "Paw patrol", summary: "Ryder and the pups are called to Adventure City to stop Mayor Humdinger from turning the bustling metropolis into a state of chaos.", year: 2021, genre: "Animation", imdb_link: "https://www.imdb.com/title/tt11832046/?ref_=fn_al_tt_4")
+Movie.create(title: "No time to die", summary: "James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.", year: 2021, genre: "Action", imdb_link: "https://www.imdb.com/title/tt2382320/?ref_=nv_sr_srsg_6")
+Movie.create(title: "Shrek", summary: "A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back.", year: 2001, genre: "Animation", imdb_link: "https://www.imdb.com/title/tt0126029/?ref_=nv_sr_srsg_0")
+Movie.create(title: "Toy Story", summary: "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy's room.", year: 1995, genre: "Animation", imdb_link: "https://www.imdb.com/title/tt0114709/?ref_=nv_sr_srsg_0")
